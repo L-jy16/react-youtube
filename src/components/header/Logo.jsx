fe818f0feddfe3b942418bd689d2fb547bdaa48c
hee031812 <144635774+L-jy16@users.noreply.github.com>
2023-10-19 15:56:58 +0900

    2023.10.19 유튜브 동영상 나오기

---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { MdMovieFilter } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+
+const Logo = () => {
+  return (
+    <h1 className='header__logo'>
+        <Link to='/'>
+            <em><MdMovieFilter/></em>
+            <span>Animation<br/>Youtube</span>
+        </Link>
+    </h1>
+  )
+}
+
+export default Logo
---
+++ src/components/section/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  	return (
+    	<footer id ='footer' role='contentinfo'>
+      		<a
+				href="mailto:lee.jiyoung492@gmail.com"
+				rel = "nonopener noreferrer"
+			>
+
+			  	mailto:lee.jiyoung492@gmail.com
+			</a>
+    	</footer>
+  	)
+}
+
+export default Footer
