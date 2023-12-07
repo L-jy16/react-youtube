12dacd45539e8922fd88c1531f701779c37cb755
jeong202 <144635774+L-jy16@users.noreply.github.com>
2023-12-07 09:03:12 +0900

    2023.12.07

--- src/components/video/VideoSearch.jsx
+++ src/components/video/VideoSearch.jsx
@@ -9,34 +9,34 @@ const formatDate = (dateString) => {
     return `${year}-${month}-${day}`;
 }
 
-const VideoSearch = ( {videos} ) => {
-  return (
-    <>
-        {videos.map((video, key) => (
-            <div className='video' key={key}>
-                <div className='video__thumb play__icon'>
-                    <Link 
-                        to={`/video/${video.id.videoId}`}
-                        style={{ backgroundImage: `url(${video.snippet.thumbnails.high.url})` }} 
-                    >
-                    </Link>
-                </div>
-                <div className='video__info'>
-                    <h3 className='title'>
-                        <Link to={`/video/${video.id.videoId}`}>{video.snippet.title}</Link>
-                    </h3> 
-                    <p className='desc'>
-                        {video.snippet.description}
-                    </p>
-                    <div className='info'>
-                        <Link to={`/channel/${video.snippet.channelId}`} className='author'>{video.snippet.channelTitle}</Link>
-                        <span className='date'>{formatDate(video.snippet.publishedAt)}</span>
+const VideoSearch = ({ videos }) => {
+    return (
+        <>
+            {videos.map((video, key) => (
+                <div className='video' key={key}>
+                    <div className='video__thumb play__icon'>
+                        <Link
+                            to={`/video/${video.id.videoId}`}
+                            style={{ backgroundImage: `url(${video.snippet.thumbnails.high.url})` }}
+                        >
+                        </Link>
+                    </div>
+                    <div className='video__info'>
+                        <h3 className='title'>
+                            <Link to={`/video/${video.id.videoId}`}>{video.snippet.title}</Link>
+                        </h3>
+                        <p className='desc'>
+                            {video.snippet.description}
+                        </p>
+                        <div className='info'>
+                            <Link to={`/channel/${video.snippet.channelId}`} className='author'>{video.snippet.channelTitle}</Link>
+                            <span className='date'>{formatDate(video.snippet.publishedAt)}</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        ))}
-    </>
-  )
+            ))}
+        </>
+    )
 }
 
 export default VideoSearch
